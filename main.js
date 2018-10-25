f337ebf2c45293ca750f70092a1b2a60367de9bc
Carlos Barrón <carlosjesus.barron@bbva.com>
2018-10-25 15:55:23 +0200

    [OOP] same circle created from factory and constructor functions

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
 // object literal sintax
-const circle = {
+// const circle = {
 
-	// properties
-	radius: 1,
-	location: {
-		x: 1,
-		y: 1
-	},
+// 	// properties
+// 	radius: 1,
+// 	location: {
+// 		x: 1,
+// 		y: 1
+// 	},
 
-	// Method (logic)
-	draw: function () {
-		console.log('draw');
-	}
-};
+// 	// Method (logic)
+// 	draw: function () {
+// 		console.log('draw');
+// 	}
+// };
 
+
+
+// To avoid to duplicate code, we can use factories
+function createCircle(radius) {
+	return {
+		// properties
+		radius,
+		location: {
+			x: 1,
+			y: 1
+		},
+		draw: function () {
+			console.log('draw from factory');
+		}
+	};
+}
+
+const circle = createCircle(1);
 circle.draw();
+
+
+// Constructor function
+function Circle(radius) {
+	// this reference the object is executing this piece of code
+	this.radius = radius;
+	this.draw = function () {
+		console.log('draw from Constructor Function');
+	}
+
+	// the return is implicit in the new Keyword when circle is created
+}
+
+const another = new Circle(1);
+
+another.draw();
