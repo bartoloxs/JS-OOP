97feec35931f92f9bf249c32e34dacafd96ae464
Carlos Barrón <carlosjesus.barron@bbva.com>
2018-10-25 15:46:56 +0200

    [OOP] sample of object literal sintax

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+// object literal sintax
+const circle = {
+
+	// properties
+	radius: 1,
+	location: {
+		x: 1,
+		y: 1
+	},
+
+	// Method (logic)
+	draw: function () {
+		console.log('draw');
+	}
+};
+
+circle.draw();
